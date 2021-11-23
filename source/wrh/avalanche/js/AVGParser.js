22edb26343cf444c57b990cac8c82197a3da99b7
Jeremy Schulz <Jeremy.Schulz@noaa.gov>
2021-11-22 17:03:51 -0700

    Fix for the word Cloud in the location name.  Cmon man!

--- source/wrh/avalanche/js/AVGParser.js
+++ source/wrh/avalanche/js/AVGParser.js
@@ -106,11 +106,12 @@ class AVGParser {
 		//Parse out the forecast points
 		const forecastData = {};
 		let avgFcsts = this.textBetweenStrings(this._productText,String.raw`^\.\.\.`,String.raw`(\n\n\n|\$\$|\*\*\*)`,'gms');
+		console.log(avgFcsts)
 		//For each parsed out forecast section, parse it out further into a location and forecast text. 
 		//perhaps we also want to parse out the elevation from the location?
 		let timesRegex = new RegExp(/^(TIME).*/im);		
 		let datesRegex = new RegExp(/^(DATE).*/im);
-		let tabularRegex = new RegExp(/(CLOUD)[\S\s]*/im)		
+		let tabularRegex = new RegExp(/^(CLOUD)[\S\s]*/im)
 		if (avgFcsts) {
 			//Because of the bass ackwards way we need to do this since safari doesnt support lookbacks,
 			//Remove it if the discussion gets in there.
