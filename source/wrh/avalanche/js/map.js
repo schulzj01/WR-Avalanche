a26be91617ae1ab1a6b5246b21be53ab5a1ade21
Jeremy Schulz <Jeremy.Schulz@noaa.gov>
2021-12-14 18:28:10 -0700

    Create priority list for event types

--- source/wrh/avalanche/js/map.js
+++ source/wrh/avalanche/js/map.js
@@ -1,5 +1,12 @@
 let mainMap,clickLayer,standardLayer; 
 
+//An list of event types ordered by priority on the map
+let queryEventTypes = ['Avalanche Warning','Avalanche Advisory','Avalanche Watch', 
+'Winter Storm Warning','Winter Weather Advisory','Winter Storm Watch','Blizzard Warning',
+'Blizzard Watch','Snow Squall Warning','Extreme Wind Warning','High Wind Warning',
+'High Wind Watch','Ice Storm Warning','Extreme Cold Warning','Extreme Cold Watch',
+'Wind Advisory','Wind Chill Advisory','Wind Chill Warning','Wind Chill Watch','Freezing Rain Advisory']
+
 function makeMap(wfo) {
   var WFO = wfo.toUpperCase();
   $.getJSON('/source/'+wfo+'/avalanche/siteConfig.json', function (cwaINFO) {
@@ -17,12 +24,16 @@ function makeMap(wfo) {
     for (i=0; i<cwaINFO.AVG_Sites.length; i++) {
       plotAVGlocations(cwaINFO.AVG_Sites[i],wfo)
     }
+
+		//Set an initial opacity on the overlays.
+		mainMap.getPane('overlayPane').style.opacity = 0.6		
+
 		//var outline = BOUNDARY.features[0].geometry;
 		var options={
 			color: '#2981ca',
 			width:'1px',
-			fillOpacity:0.8,			
-			fillColor: 'gray',
+			fillOpacity:1,
+			fillColor: '#303030',
 			restrictBounds: false,
 			fitBounds: false,
 		};
@@ -57,7 +68,7 @@ function plotAVGlocations(locationData,wfo) {
   } else if (type == 'ShapeFile') {
     $.getJSON('/source/'+wfo+'/avalanche/'+locationData.geometry.coordinates, function(shape) {
       var outline = shape.features[0].geometry;
-      var color_style={"color": "blue", "fillColor":"blue","fillOpacity":0.5,"width":"1px"};
+      var color_style={"color": "blue", "fillColor":"blue","fillOpacity":0.8,"width":"1px"};
       var border = L.geoJson(outline, {style: color_style});
 			border.locationId = locationData.location.toLowerCase();
 			border.layerType = 'polygon';			
@@ -77,42 +88,39 @@ function queryWWA(WFO){
 	//Alerts filtered out by active CWA. We probably want to move this to a map call? Or better yet, use it to populate the map with the CWA hazards?
 	let cwaAlerts = new NwsApi.Alert({
 		active: true,
-		event: ['Avalanche Advisory','Avalanche Warning','Avalanche Watch', 
-						'Winter Storm Advisory','Winter Storm Warning','Winter Storm Watch',
-						'Wind Advisory','Wind Chill Advisory','Wind Chill Warning','Wind Chill Watch',
-						'Winter Weather Advisory','High Wind Warning','High Wind Watch','Extreme Wind Warning',
-						'Ice Storm Warning','Extreme Cold Warning','Extreme Cold Watch',
-						'Blizzard Warning','Blizzard Watch','Snow Squall Warning','Freezing Rain Advisory']
-	}).getByCwa(WFO,getWwa,WFO);	 //TODO Make sure to switch back to CWA filtered WWAs before going live.
+		event: queryEventTypes
+	}).getByCwa(WFO,getWwa,WFO);	 
 	//}).getAll(getWWA,WFO); //Switch to getAll instead of getByCwa to get all alerts over the country.  Good for testing.
 }
 
 // Get Winter Wx Specific WWA for the CWA, plot on map
 function getWwa(WWA,WFO) {
   $.getJSON('/source/slc/common_data/support.json', function (support) {
-		//TODO  Do we need to specify that these are only winter/avalanche relatd hazards?
 		var Legend = '<table bgcolor="white" border="1px"><tr><td colspan="2">Winter Related Watches & Warnings<br>Displayed Only in the Highlighted Area'; 
 		var NUM = WWA.features.length;
 		if (NUM != "0") {
-			for (i=0; i<NUM; i++) {
-				var Phenom = (WWA.features[i].properties.event);					
+			//Sort hazards into lowest to highest priority so the highest priority show up on the map last.
+			let sortedWWAFeatures = WWA.features.sort((featA,featB) => queryEventTypes.indexOf(featB.properties.event) - queryEventTypes.indexOf(featA.properties.event));
+			sortedWWAFeatures.forEach(feat => {
+				let props = feat.properties;
+				var Phenom = (props.event);					
 				for (m=0; m < support.fill.length; m++) {
 					if (Phenom == support.fill[m].product) {
 						FC = support.fill[m].hex;
-						var ZONES = WWA.features[i].properties.affectedZones.length;
-						var description =  WWA.features[i].properties.description.replace(/(?:\r\n|\r|\n)/g,"<br>&nbsp;");
+						var ZONES = props.affectedZones.length;
+						var description = props.description.replace(/(?:\r\n|\r|\n)/g,"<br>&nbsp;");
 						if (Legend.includes(Phenom)) {
 							Legend += '' 
 						} else {
 							Legend += '<tr><td bgcolor="'+FC+'" width="10px">&nbsp;</td><td>'+Phenom+'</td></tr>';
 						}
 						for (j=0; j< ZONES; j++) {
-							var Affected = (WWA.features[i].properties.affectedZones[j])
-							showCountyZone(Affected,FC,WWA.features[i].properties)
+							var Affected = (props.affectedZones[j])
+							showCountyZone(Affected,FC,props)
 						}
 					} 
 				}
-			}
+			});
 			Legend += '</table>';
 			legend(Legend);
 		} else {
@@ -126,13 +134,13 @@ function getWwa(WWA,WFO) {
 // Polygon based warnings
 function showPolygon (DATA,COLOR,OPAC) {
   if (!standardLayer) {
-    standardLayer = L.layerGroup().addTo(mainMap);
+    standardLayer = L.featureGroup().addTo(mainMap);
   }
   plot = DATA.features[0].geometry;
   var originalMsg = JSON.stringify(plot);
   originalMsg = originalMsg.replace('rings','Polygon');
   var newObj = JSON.parse(originalMsg); 
-  var color_style={"color": COLOR, "weight":2,"fillColor":COLOR,"fillOpacity": OPAC};
+  var color_style={"color": COLOR, "weight":2,"fillColor":COLOR, fillOpacity: 1};
   var foreFront = L.geoJson(newObj, {style: color_style});
   foreFront.addTo(standardLayer);
 };
@@ -140,10 +148,10 @@ function showPolygon (DATA,COLOR,OPAC) {
 // COunty/Zone based warnings
 function showCountyZone (LOCATION,COLOR,alertProduct) {
   if (!standardLayer) {
-    standardLayer = L.layerGroup().addTo(mainMap);
+    standardLayer = L.featureGroup().addTo(mainMap);
   }
   $.getJSON(LOCATION, function(plot) {
-    var color_style={"color": COLOR, "weight":0,"fillColor":COLOR,"fillOpacity": 0.5};
+    var color_style={"color": COLOR, "weight":0,"fillColor":COLOR, fillOpacity: 1};
     var foreFront = L.geoJson(plot, {
 			style: color_style,
 			interactive:false,
